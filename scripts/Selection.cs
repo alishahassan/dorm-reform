5a9f636ea4e4424816110d1dbbc44ccb25d34a40
vivienlatt1010 <114167259+vivienlatt1010@users.noreply.github.com>
2025-04-15 19:37:17 -0400

    Update and rename selection.cs to Selection.cs

--- scripts/Selection.cs
+++ scripts/Selection.cs
@@ -2,38 +2,42 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class selection : MonoBehaviour
+public class Selection : MonoBehaviour
 {
    
    public GameObject selectedObject;
    public TextMeshProUGUI objNameText;
-   private buildingmanager buildingManager;
+   private BuildingManager buildingManager;
 
    public GameObject objUi;
    
     void Start()
     {
-        buildingManager = GameObject.Find("buildingmanager").GetComponent<buildingmanager>();
+        buildingManager = GameObject.Find("BuildingManager").GetComponent<buildingmanager>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 1000)) {
-                if(hit.collider.gameObject.CompareTag("object")) {
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                if(hit.collider.gameObject.CompareTag("object"))
+                {
                     Select(hit.collider.gameObject);
                 }
             }
         }
-        if(Input.GetMouseButtonDown(1) && selectedObject != null) {
+        if(Input.GetMouseButtonDown(1) && selectedObject != null)
+        {
             Deselect();
         }
-        
     }
 
-    private void Select(GameObject obj) {
+    private void Select(GameObject obj)
+    {
         if(obj == selectedObject) return;
         if(selectedObject != null) Deselect();
         Outline outline = obj.GetComponent<Outline>();
@@ -43,24 +47,28 @@ public class selection : MonoBehaviour
         selectedObject = obj;
     }
 
-    private void Deselect() {
+    private void Deselect()
+    {
         objUi.SetActive(false);
         selectedObject.GetComponent<Outline>().enabled = false;
         selectedObject = null;
     }
     
-    public void Move() {
+    public void Move()
+    {
         buildingManager.pendingObject = selectedObject;
     }
 
-    public void Rotate() {
-    if (selectedObject != null) {
+    public void Rotate()
+    {
+    if (selectedObject != null)
+    {
         selectedObject.transform.Rotate(Vector3.up, 90f);
     }
 }
 
-
-    public void Delete() {
+    public void Delete()
+    {
         GameObject objToDestroy = selectedObject;
         Deselect();
         Destroy(objToDestroy);
